8a0a68dff82cc10548a19b872b442e3af665d287
IMBor1 <ilacerber1@gmail.com>
2023-10-13 22:02:04 +0300

    сделал ДЗ №5

--- src/Main.java
+++ src/Main.java
@@ -51,7 +51,7 @@ public class Main {
             System.out.println("доставки нет");
         }
 
-        // Задание № 5
+        // Задание №5
         int monthNumber = 12;
         switch (monthNumber) {
             case 12:
